817e780a03b8e38a872b2f498cb000eda8ef2e60
davidtsoy-hkclr <177311445+davidtsoy-hkclr@users.noreply.github.com>
2024-12-17 11:14:10 +0800

    Adjusted x264 encoder settings

--- ros2_h264_encoder/include/ros2_h264_encoder/ros2_encoder.hpp
+++ ros2_h264_encoder/include/ros2_h264_encoder/ros2_encoder.hpp
@@ -1,72 +1,88 @@
 #ifndef ROS2_ENCODER_H
 #define ROS2_ENCODER_H
 // System Includes
+#include <chrono>
 #include <string>
 #include <unordered_map>
-#include <chrono>
 
 // External ROS Includes
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "sensor_msgs/image_encodings.hpp"
+#include <rclcpp/rclcpp.hpp>
+
 #include "h264_msgs/msg/packet.hpp"
+#include "sensor_msgs/image_encodings.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 // External Includes
-extern "C"
-{
-#include <x264.h>
+extern "C" {
 #include <libswscale/swscale.h>
+#include <x264.h>
 }
 
-#define MAX_FRAME_RATE_SAMPLES 10
+#define MAX_FRAME_RATE_SAMPLES 30
 
 class ROS2Encoder {
-public:
-    ROS2Encoder() : logger(rclcpp::get_logger("H264PublisherEncoder")) {}
-    ROS2Encoder(rclcpp::Logger logger);
-    ~ROS2Encoder();
-    bool encode_image(const sensor_msgs::msg::Image &msg, h264_msgs::msg::Packet &packet);
-    bool convert_image_to_h264(const sensor_msgs::msg::Image &msg, x264_picture_t* out);
-    int get_seq() {
-        return seq;
-    }
+ public:
+  ROS2Encoder(rclcpp::Logger logger);
 
-private:
-    // Encoder variables
-    x264_t* encoder;
-    x264_param_t params;
+  ~ROS2Encoder();
+  bool encode_image(const sensor_msgs::msg::Image& msg,
+                    h264_msgs::msg::Packet& packet);
+  bool convert_image_to_h264(const sensor_msgs::msg::Image& msg,
+                             x264_picture_t* out);
 
-    x264_picture_t input;
-    x264_picture_t output;
-    x264_nal_t* nals;
-    int i_nals;
-    int64_t pts;
+  constexpr int get_seq() const { return frame_sequence_; }
 
-    int seq;
-    
-    // Conversion
-    struct SwsContext* conversion_context;
+  using ClockBase = std::chrono::high_resolution_clock;
+  template <typename T, typename _Per = std::ratio<1>>
+  using Duration = std::chrono::duration<T,_Per>;
 
-    static std::unordered_map<std::string, AVPixelFormat> create_ROS_encoding_to_AV_Pixel_format() {
-        std::unordered_map<std::string, AVPixelFormat> formats;
-        formats[sensor_msgs::image_encodings::BGR16] = AV_PIX_FMT_BGR48;
-        formats[sensor_msgs::image_encodings::BGR8] = AV_PIX_FMT_BGR24;
-        formats[sensor_msgs::image_encodings::BGRA16] = AV_PIX_FMT_BGRA64;
-        formats[sensor_msgs::image_encodings::BGRA8] = AV_PIX_FMT_BGRA;
-        formats[sensor_msgs::image_encodings::MONO16] = AV_PIX_FMT_GRAY16;
-        formats[sensor_msgs::image_encodings::MONO8] = AV_PIX_FMT_GRAY8;
-        formats[sensor_msgs::image_encodings::RGB16] = AV_PIX_FMT_RGB48;
-        formats[sensor_msgs::image_encodings::RGB8] = AV_PIX_FMT_RGB24;        
-        formats[sensor_msgs::image_encodings::RGBA16] = AV_PIX_FMT_RGBA64;
-        formats[sensor_msgs::image_encodings::RGBA8] = AV_PIX_FMT_RGBA;
-        return formats;
-    }
+  template <typename T>
+  using DurationUs = Duration<T, std::micro>;
+  template <typename T>
+  using DurationMs = Duration<T, std::milli>;
 
-    static const std::unordered_map<std::string, AVPixelFormat> ROS_encoding_to_AV_Pixel_format;
+ private:
+  // Encoder variables
+  x264_t* encoder_handle_;
+  x264_param_t encoder_params_;
 
-    // ROS
-    rclcpp::Logger logger;
+  x264_picture_t input;
+  x264_picture_t output;
+  x264_nal_t* nals_;
+  int i_nals_;
+  int64_t pts_;
+
+  int frame_sequence_;
+
+  // flag to indicate sampling of video stream
+  bool is_sampling_;
+  ClockBase::time_point last_frame_time_;
+  DurationUs<double> frame_time_delta_;
+
+  // Conversion
+  struct SwsContext* p_conversion_context_;
+
+  static std::unordered_map<std::string, AVPixelFormat>
+  create_ROS_encoding_to_AV_Pixel_format() {
+    std::unordered_map<std::string, AVPixelFormat> formats;
+    formats[sensor_msgs::image_encodings::BGR16] = AV_PIX_FMT_BGR48;
+    formats[sensor_msgs::image_encodings::BGR8] = AV_PIX_FMT_BGR24;
+    formats[sensor_msgs::image_encodings::BGRA16] = AV_PIX_FMT_BGRA64;
+    formats[sensor_msgs::image_encodings::BGRA8] = AV_PIX_FMT_BGRA;
+    formats[sensor_msgs::image_encodings::MONO16] = AV_PIX_FMT_GRAY16;
+    formats[sensor_msgs::image_encodings::MONO8] = AV_PIX_FMT_GRAY8;
+    formats[sensor_msgs::image_encodings::RGB16] = AV_PIX_FMT_RGB48;
+    formats[sensor_msgs::image_encodings::RGB8] = AV_PIX_FMT_RGB24;
+    formats[sensor_msgs::image_encodings::RGBA16] = AV_PIX_FMT_RGBA64;
+    formats[sensor_msgs::image_encodings::RGBA8] = AV_PIX_FMT_RGBA;
+    return formats;
+  }
+
+  static const std::unordered_map<std::string, AVPixelFormat>
+      ROS_encoding_to_AV_Pixel_format;
+
+  // ROS
+  rclcpp::Logger logger_;
 };
 
 #endif
-
--- ros2_h264_encoder/src/ros2_encoder.cpp
+++ ros2_h264_encoder/src/ros2_encoder.cpp
@@ -4,64 +4,104 @@
 // Project Includes
 #include "ros2_h264_encoder/ros2_encoder.hpp"
 
-const std::unordered_map<std::string, AVPixelFormat> ROS2Encoder::ROS_encoding_to_AV_Pixel_format = ROS2Encoder::create_ROS_encoding_to_AV_Pixel_format();
+const std::unordered_map<std::string, AVPixelFormat>
+    ROS2Encoder::ROS_encoding_to_AV_Pixel_format =
+        ROS2Encoder::create_ROS_encoding_to_AV_Pixel_format();
 
-ROS2Encoder::ROS2Encoder(rclcpp::Logger logger) : pts(0), seq(0), logger(logger) {
-    x264_param_default_preset(&params, "veryfast", "zerolatency");
-    params.i_threads = 1;
-    params.i_fps_den = 1;
+ROS2Encoder::ROS2Encoder(rclcpp::Logger logger)
+    : pts_(0), frame_sequence_(0), is_sampling_(true)
+    , last_frame_time_(), frame_time_delta_(0.0),logger_(logger) {
+  x264_param_default_preset(&encoder_params_, "veryfast", "zerolatency");
+  encoder_params_.b_repeat_headers = 1;
+  encoder_params_.i_threads = 1;
+  encoder_params_.i_fps_den = 1;
 }
 
 ROS2Encoder::~ROS2Encoder() {
-    x264_picture_clean(&input);
-    x264_encoder_close(encoder);
-    sws_freeContext(conversion_context);
+  x264_picture_clean(&input);
+  x264_encoder_close(encoder_handle_);
+  sws_freeContext(p_conversion_context_);
 }
-    
-bool ROS2Encoder::encode_image(const sensor_msgs::msg::Image &msg, h264_msgs::msg::Packet &packet) {
-    if (!encoder) {
-            RCLCPP_INFO_STREAM(logger, "Creating encoder with image height: " << msg.height << ", image width: " << msg.width << " and encoding " << msg.encoding);
-            
-            params.i_width = msg.width;
-            params.i_height = msg.height;
 
-            encoder = x264_encoder_open(&params);
-            if (!encoder) {
-                RCLCPP_WARN_STREAM(logger, "Could not open encoder!");
-            }
+bool ROS2Encoder::encode_image(const sensor_msgs::msg::Image &msg,
+                               h264_msgs::msg::Packet &packet) {
+  if (is_sampling_) {
+    if (frame_sequence_ == 0) {
+      last_frame_time_ = ClockBase::now();
+    }
+    frame_time_delta_ = (frame_time_delta_ + (ClockBase::now() - last_frame_time_)) / 2.0;
+    last_frame_time_ = ClockBase::now();
+    frame_sequence_++;
 
-            if(x264_picture_alloc(&input, X264_CSP_I420, params.i_width, params.i_height)){
-                RCLCPP_WARN_STREAM(logger, "Cannot allocate x264 picure");
-            }
-            
-            conversion_context = sws_getContext(msg.width, msg.height, ROS_encoding_to_AV_Pixel_format.at(msg.encoding), 
-                    msg.width, msg.height, AV_PIX_FMT_YUV420P, SWS_FAST_BILINEAR, NULL, NULL, NULL);
-            if (!conversion_context) {
-                RCLCPP_WARN_STREAM(logger, "Failed to get conversion context!");
-            }            
-            RCLCPP_INFO_STREAM(logger, "Finished creating encoder with FPS image height: " << msg.height << ", image width: " << msg.width << " and encoding " << msg.encoding);
+    if (frame_sequence_ >= MAX_FRAME_RATE_SAMPLES) {
+      auto fps = 1.0 / Duration<double>(frame_time_delta_).count();
+      RCLCPP_INFO_STREAM(logger_, "Avg Frame Time:" << frame_time_delta_.count() << ", " << "FPS: " << fps);
+      // change encoder FPS to sampled interval
+      this->encoder_params_.i_fps_num = static_cast<uint32_t>(std::round(fps));
+      this->encoder_params_.i_keyint_max = 2 * static_cast<uint32_t>(std::round(fps));
+      is_sampling_ = false;
+    } else {
+      return false;
     }
-    seq++;
-    if (convert_image_to_h264(msg, &input)) {
-        int frame_size = x264_encoder_encode(encoder, &nals, &i_nals, &input, &output);
-        packet.seq = pts;
-        pts++;
-        if (frame_size >= 0) {
-            for (int i = 0; i < i_nals; i++) {
-                std::copy(nals[i].p_payload, nals[i].p_payload + nals[i].i_payload, std::back_inserter(packet.data));
-            }
-            return true;
-        } else {
-            RCLCPP_WARN_STREAM(logger, "Could not encode image!");
-        }
+  }
+
+  if (!encoder_handle_) {
+    RCLCPP_INFO_STREAM(logger_, "Creating x264 Encoder with Image: "
+                                    << msg.width << "x" << msg.height << ", "
+                                    << "Encoding: " << msg.encoding << ", "
+                                    << encoder_params_.i_fps_num << " FPS");
+
+    encoder_params_.i_width = msg.width;
+    encoder_params_.i_height = msg.height;
+
+    encoder_handle_ = x264_encoder_open(&encoder_params_);
+    if (!encoder_handle_) {
+      RCLCPP_WARN_STREAM(logger_, "Could not open encoder!");
     }
-    return false;
+
+    if (x264_picture_alloc(&input, X264_CSP_I420, encoder_params_.i_width,
+                           encoder_params_.i_height)) {
+      RCLCPP_WARN_STREAM(logger_, "Cannot allocate x264 picture");
+    }
+
+    p_conversion_context_ = sws_getContext(
+        msg.width, msg.height, ROS_encoding_to_AV_Pixel_format.at(msg.encoding),
+        msg.width, msg.height, AV_PIX_FMT_YUV420P, SWS_FAST_BILINEAR, NULL,
+        NULL, NULL);
+    if (!p_conversion_context_) {
+      RCLCPP_WARN_STREAM(logger_, "Failed to get conversion context!");
+    }
+    RCLCPP_INFO_STREAM(logger_, "Successfully created X264 Encoder with Image: "
+                                    << msg.width << "x" << msg.height << ", "
+                                    << "Encoding:" << msg.encoding << ", "
+                                    << encoder_params_.i_fps_num << " FPS");
+  }
+  frame_sequence_++;
+  if (convert_image_to_h264(msg, &input)) {
+    int frame_size =
+        x264_encoder_encode(encoder_handle_, &nals_, &i_nals_, &input, &output);
+    packet.seq = pts_;
+    pts_++;
+    if (frame_size >= 0) {
+      for (int i = 0; i < i_nals_; i++) {
+        std::copy(nals_[i].p_payload, nals_[i].p_payload + nals_[i].i_payload,
+                  std::back_inserter(packet.data));
+      }
+      return true;
+    } else {
+      RCLCPP_WARN_STREAM(logger_, "Could not encode image!");
+    }
+  }
+  return false;
 }
-    
-bool ROS2Encoder::convert_image_to_h264(const sensor_msgs::msg::Image &msg, x264_picture_t* out) {
-    int stride[3] = {static_cast<int>(msg.width) * 3, 0, 0};
-    uint8_t *src[3]= {const_cast<uint8_t *>(&msg.data[0]), NULL, NULL};
-    int returnedHeight = sws_scale(conversion_context, src, stride, 0, params.i_height, out->img.plane, out->img.i_stride);
-    out->i_pts = pts;
-    return returnedHeight == params.i_height;
+
+bool ROS2Encoder::convert_image_to_h264(const sensor_msgs::msg::Image &msg,
+                                        x264_picture_t *out) {
+  int stride[3] = {static_cast<int>(msg.width) * 3, 0, 0};
+  uint8_t *src[3] = {const_cast<uint8_t *>(&msg.data[0]), NULL, NULL};
+  int returnedHeight =
+      sws_scale(p_conversion_context_, src, stride, 0, encoder_params_.i_height,
+                out->img.plane, out->img.i_stride);
+  out->i_pts = pts_;
+  return returnedHeight == encoder_params_.i_height;
 }
